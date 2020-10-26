308bc3c6a5523f9e464bb790a71c1ef1fc1e36d4
Davide <kalelc351k@gmail.com>
2020-10-26 23:45:12 +0100

    logica completata

--- js/script.js
+++ js/script.js
@@ -5,43 +5,18 @@ for (var i = 1; i < 101; i++) {
   var multiplo3 = i % 3
   var multiplo5 = i % 5
 
-  if (multiplo3 !== 0) {
+  // condizioni per mostrare fizz e buzz
+  if ((multiplo3 == 0) && (multiplo5 == 0)) {
+    console.log("FizzBuzz");
+  } else if (multiplo3 == 0) {
+    console.log("Fizz");
+  } else if (multiplo5 == 0) {
+    console.log('Buzz');
+  } else {
     console.log(i);
   }
-
-  // if (multiplo5 !== 0) {
-  //   console.log(i);
-  // }
-
-  if (multiplo3 == 0) {
-    console.log("Fizz");
-  }
-
-  if (multiplo5 == 0) {
-    console.log('Buzz');
-  }
-  // if (multiplo3 == 0) && (multiplo5 == 0) {
-  //   console.log('multiplo sia di 5 che di 3');
-  // }
 }
 
 // Scrivi un programma che stampi i numeri da 1 a 100,
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-
-
-
-
-
-
-
-
-
-
-
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Come faccio a sapere se un numero è divisibile per?
-// Abbiamo visto qualcosa di particolare che possiamo usare?
-// Consigli del giorno:
-// 1. scriviamo sempre prima dei commenti in italiano (o meglio ancora in inglese se vi ci trovate) per capire cosa vogliamo fare
-// 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo “a mano”
